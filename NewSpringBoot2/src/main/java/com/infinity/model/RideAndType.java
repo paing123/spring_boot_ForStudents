9eb5051f4fcae990a0b6c040b35ad47f49074c6c
paing123 <paingpaing4228@gmail.com>
2019-11-10 22:27:45 +0630

    join testing complete

--- NewSpringBoot2/src/main/java/com/infinity/model/RideAndType.java
+++ NewSpringBoot2/src/main/java/com/infinity/model/RideAndType.java
@@ -1,40 +1,42 @@
 package com.infinity.model;
 
-public class Ride {
+public class RideAndType {
 	public Integer id;
 	public String name;
 	public String duration;
+	public Integer rideTypeId;
 	public String rideType;
 	
-	public String getDuration() {
-		return duration;
-	}
-
-	public void setDuration(String duration) {
-		this.duration = duration;
-	}
-
 	public Integer getId() {
 		return id;
 	}
-
 	public void setId(Integer id) {
 		this.id = id;
 	}
-
 	public String getName() {
 		return name;
 	}
-
 	public void setName(String name) {
 		this.name = name;
 	}
-
+	public String getDuration() {
+		return duration;
+	}
+	public void setDuration(String duration) {
+		this.duration = duration;
+	}
+	public Integer getRideTypeId() {
+		return rideTypeId;
+	}
+	public void setRideTypeId(Integer rideTypeId) {
+		this.rideTypeId = rideTypeId;
+	}
 	public String getRideType() {
 		return rideType;
 	}
-
 	public void setRideType(String rideType) {
 		this.rideType = rideType;
 	}
+	
+	
 }
